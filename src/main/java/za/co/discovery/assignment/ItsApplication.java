8be57656bb9274bc752f88abe4efff7dabe420a6
freddy <sopfreddy@gmail.com>
2020-08-26 21:04:58 +0200

    initial project setup

---
+++ src/main/java/za/co/discovery/assignment/ItsApplication.java
@@ -0,0 +1,13 @@
+package za.co.discovery.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsApplication.class, args);
+	}
+
+}
