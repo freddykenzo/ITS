202bf248ac503c6b1c0812c7225895044dec2973
freddy <sopfreddy@gmail.com>
2020-08-29 15:53:54 +0200

    add dependencies for test coverage

--- src/test/java/za/co/discovery/assignment/ItsApplicationTest.java
+++ src/test/java/za/co/discovery/assignment/ItsApplicationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ItsApplicationTests {
+class ItsApplicationTest {
 
 	@Test
 	void contextLoads() {
